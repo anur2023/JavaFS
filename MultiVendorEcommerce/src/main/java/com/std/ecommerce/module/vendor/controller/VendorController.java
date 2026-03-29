5b8c3e64df2a906807074be2db82f7be86b433b6
Aparna368 <tiwariaparna776@gmail.com>
2026-03-29 16:20:45 +0530

    folder

---
+++ MultiVendorEcommerce/src/main/java/com/std/ecommerce/module/vendor/controller/VendorController.java
@@ -0,0 +1,4 @@
+package com.std.ecommerce.module.vendor.controller;
+
+public class VendorController {
+}
---
+++ MultiVendorEcommerce/src/main/java/com/std/ecommerce/module/vendor/repository/VendorRepo.java
@@ -0,0 +1,4 @@
+package com.std.ecommerce.module.vendor.repository;
+
+public class VendorRepo {
+}
