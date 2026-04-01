b8dcac06191fae24d5cb6a3e7128d6c10e5af05f
Aparna368 <tiwariaparna776@gmail.com>
2026-04-01 13:57:11 +0530

    new project

---
+++ FashionE-CommercewithVirtualTry-On/src/main/java/com/example/FashionE_CommercewithVirtualTry_On/FashionECommercewithVirtualTryOnApplication.java
@@ -0,0 +1,13 @@
+package com.example.FashionE_CommercewithVirtualTry_On;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionECommercewithVirtualTryOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionECommercewithVirtualTryOnApplication.class, args);
+	}
+
+}
---
+++ FashionE-CommercewithVirtualTry-On/src/test/java/com/example/FashionE_CommercewithVirtualTry_On/FashionECommercewithVirtualTryOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FashionE_CommercewithVirtualTry_On;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionECommercewithVirtualTryOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
